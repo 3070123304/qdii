bac97e0838a7bd7376c4a3850faca52cdcf86537
3070123304 <3070123304@qq.com>
2026-06-19 15:30:48 +0800

    Modify mockData.js with updated market and fund info
    Updated mock data with new market index values and fund returns.

--- mockData.js
+++ mockData.js
@@ -1,5 +1,5 @@
 window.mockData = {
-  updateDateLabel: "2026-06-05 12:00",
+  updateDateLabel: "2026-06-019 12:00",
 
   previousSummary: {
     buyableCount: 29,
@@ -7,11 +7,11 @@ window.mockData = {
   },
 
   glanceTitle: "今日速览",
-  glance: "纳指QDII缩减额度",
+  glance: "QDII限额告急",
 
   marketIndexes: [
-    { name: "纳斯达克", value: "30407", change: "-0.53%" },
-    { name: "标普500", value: "7584", change: "+0.41%" }
+    { name: "纳斯达克100", value: "30406", change: "+2.48%" },
+    { name: "标普500", value: "7500", change: "+1.08%" }
   ],
 
   fundCategories: [
@@ -20,54 +20,59 @@ window.mockData = {
   ],
 
   funds: [
-    { id: "1", category: "theme", name: "汇添富纳斯达克生物科技ETF发起式联接(QDII)A", code: "017894", limitText: "5000元", oneYearReturn: "+27.40%", updatedAt: "08:00 06-03" },
-    { id: "2", category: "theme", name: "汇添富纳斯达克生物科技ETF发起式联接(QDII)C", code: "017895", limitText: "5000元", oneYearReturn: "+27.07%", updatedAt: "08:00 06-03" },
+    { id: "1", category: "theme", name: "汇添富纳斯达克生物科技ETF发起式联接(QDII)A", code: "017894", limitText: "5000元", oneYearReturn: "+32.15%" },
+    { id: "2", category: "theme", name: "汇添富纳斯达克生物科技ETF发起式联接(QDII)C", code: "017895", limitText: "5000元", oneYearReturn: "+31.81%" },
 
-    { id: "3", category: "broad", name: "南方纳斯达克100指数(QDII)A", code: "016452", limitText: "200元", oneYearReturn: "+31.15%", updatedAt: "08:00 06-03" },
-    { id: "4", category: "broad", name: "南方纳斯达克100指数(QDII)C", code: "016453", limitText: "200元", oneYearReturn: "+31.02%", updatedAt: "08:00 06-03" },
+    { id: "3", category: "broad", name: "建信纳斯达克100指数(QDII)A人民币", code: "539001", limitText: "100元", oneYearReturn: "+26.37%" },
+    { id: "4", category: "broad", name: "建信纳斯达克100指数(QDII)C人民币", code: "012752", limitText: "100元", oneYearReturn: "+25.99%" },
+    { id: "5", category: "broad", name: "建信纳斯达克100指数(QDII)D人民币", code: "023422", limitText: "100元", oneYearReturn: "+25.99%" },
 
-    { id: "5", category: "broad", name: "摩根纳斯达克100指数(QDII)人民币A", code: "019172", limitText: "100元", oneYearReturn: "+31.45%", updatedAt: "08:00 06-03" },
-    { id: "6", category: "broad", name: "摩根纳斯达克100指数(QDII)人民币C", code: "019173", limitText: "100元", oneYearReturn: "+31.06%", updatedAt: "08:00 06-03" },
-    { id: "7", category: "broad", name: "招商纳斯达克100ETF发起式联接(QDII)A", code: "019547", limitText: "100元", oneYearReturn: "+30.84%", updatedAt: "08:00 06-03" },
-    { id: "8", category: "broad", name: "宝盈纳斯达克100指数发起(QDII)A人民币", code: "019736", limitText: "100元", oneYearReturn: "+30.52%", updatedAt: "08:00 06-03" },
-    { id: "9", category: "broad", name: "招商纳斯达克100ETF发起式联接(QDII)C", code: "019548", limitText: "100元", oneYearReturn: "+30.31%", updatedAt: "08:00 06-03" },
-    { id: "10", category: "broad", name: "建信纳斯达克100指数(QDII)A人民币", code: "539001", limitText: "100元", oneYearReturn: "+30.30%", updatedAt: "08:00 06-03" },
-    { id: "11", category: "broad", name: "宝盈纳斯达克100指数发起(QDII)C人民币", code: "019737", limitText: "100元", oneYearReturn: "+30.20%", updatedAt: "08:00 06-03" },
-    { id: "12", category: "broad", name: "建信纳斯达克100指数(QDII)D人民币", code: "023422", limitText: "100元", oneYearReturn: "+29.91%", updatedAt: "08:00 06-03" },
-    { id: "13", category: "broad", name: "建信纳斯达克100指数(QDII)C人民币", code: "012752", limitText: "100元", oneYearReturn: "+29.90%", updatedAt: "08:00 06-03" },
-    { id: "14", category: "broad", name: "汇添富纳斯达克100ETF发起式联接(QDII)A", code: "018966", limitText: "100元", oneYearReturn: "+29.55%", updatedAt: "08:00 06-03" },
-    { id: "15", category: "broad", name: "汇添富纳斯达克100ETF发起式联接(QDII)E", code: "021773", limitText: "100元", oneYearReturn: "+29.42%", updatedAt: "08:00 06-03" },
-    { id: "16", category: "broad", name: "汇添富纳斯达克100ETF发起式联接(QDII)C", code: "018967", limitText: "100元", oneYearReturn: "+29.02%", updatedAt: "08:00 06-03" },
+    { id: "6", category: "broad", name: "汇添富纳斯达克100ETF发起式联接(QDII)A", code: "018966", limitText: "100元", oneYearReturn: "+25.65%" },
+    { id: "7", category: "broad", name: "汇添富纳斯达克100ETF发起式联接(QDII)E", code: "021773", limitText: "100元", oneYearReturn: "+25.53%" },
+    { id: "8", category: "broad", name: "汇添富纳斯达克100ETF发起式联接(QDII)C", code: "018967", limitText: "100元", oneYearReturn: "+25.16%" },
 
-    { id: "17", category: "broad", name: "万家纳斯达克100指数发起式(QDII)A", code: "019441", limitText: "50元", oneYearReturn: "+30.33%", updatedAt: "08:00 06-03" },
-    { id: "18", category: "broad", name: "万家纳斯达克100指数发起式(QDII)C", code: "019442", limitText: "50元", oneYearReturn: "+30.07%", updatedAt: "08:00 06-03" },
+    { id: "9", category: "broad", name: "南方纳斯达克100指数(QDII)A", code: "016452", limitText: "50元", oneYearReturn: "+27.10%" },
+    { id: "10", category: "broad", name: "南方纳斯达克100指数(QDII)C", code: "016453", limitText: "50元", oneYearReturn: "+26.97%" },
 
-    { id: "19", category: "broad", name: "华安纳斯达克100ETF联接A", code: "040046", limitText: "10元", oneYearReturn: "+32.79%", updatedAt: "08:00 06-03" },
-    { id: "20", category: "broad", name: "华安纳斯达克100ETF联接C", code: "014978", limitText: "10元", oneYearReturn: "+32.53%", updatedAt: "08:00 06-03" },
-    { id: "21", category: "broad", name: "广发纳斯达克100ETF联接人民币(QDII)A", code: "270042", limitText: "10元", oneYearReturn: "+32.18%", updatedAt: "08:00 06-03" },
-    { id: "22", category: "broad", name: "大成纳斯达克100ETF联接(QDII)A", code: "000834", limitText: "10元", oneYearReturn: "+32.03%", updatedAt: "08:00 06-03" },
-    { id: "23", category: "broad", name: "广发纳斯达克100ETF联接人民币(QDII)C", code: "006479", limitText: "10元", oneYearReturn: "+31.92%", updatedAt: "08:00 06-03" },
-    { id: "24", category: "broad", name: "大成纳斯达克100ETF联接(QDII)C", code: "008971", limitText: "10元", oneYearReturn: "+31.63%", updatedAt: "08:00 06-03" },
-    { id: "25", category: "broad", name: "华泰柏瑞纳斯达克100ETF发起式联接(QDII)A", code: "019524", limitText: "10元", oneYearReturn: "+30.38%", updatedAt: "08:00 06-03" },
-    { id: "26", category: "broad", name: "华泰柏瑞纳斯达克100ETF发起式联接(QDII)I", code: "022664", limitText: "10元", oneYearReturn: "+30.19%", updatedAt: "08:00 06-03" },
-    { id: "27", category: "broad", name: "华泰柏瑞纳斯达克100ETF发起式联接(QDII)C", code: "019525", limitText: "10元", oneYearReturn: "+30.05%", updatedAt: "08:00 06-03" },
-    { id: "28", category: "theme", name: "广发生物科技指数人民币(QDII)A", code: "001092", limitText: "10元", oneYearReturn: "+26.55%", updatedAt: "08:00 06-03" },
-    { id: "29", category: "theme", name: "广发生物科技指数人民币(QDII)C", code: "016470", limitText: "10元", oneYearReturn: "+26.03%", updatedAt: "08:00 06-03" },
+    { id: "11", category: "broad", name: "万家纳斯达克100指数发起式(QDII)A", code: "019441", limitText: "50元", oneYearReturn: "+26.48%" },
+    { id: "12", category: "broad", name: "万家纳斯达克100指数发起式(QDII)C", code: "019442", limitText: "50元", oneYearReturn: "+26.22%" },
 
-    { id: "30", category: "theme", name: "景顺长城纳斯达克科技ETF联接(QDII)A人民币", code: "017091", limitText: "0元", oneYearReturn: "+48.17%", statusText: "暂停买入", updatedAt: "08:00 06-03" },
-    { id: "31", category: "theme", name: "景顺长城纳斯达克科技ETF联接(QDII)E人民币", code: "019118", limitText: "0元", oneYearReturn: "+47.87%", statusText: "暂停买入", updatedAt: "08:00 06-03" },
-    { id: "32", category: "theme", name: "景顺长城纳斯达克科技ETF联接(QDII)C人民币", code: "017093", limitText: "0元", oneYearReturn: "+47.58%", statusText: "暂停买入", updatedAt: "08:00 06-03" },
+    { id: "13", category: "theme", name: "广发生物科技指数人民币(QDII)A", code: "001092", limitText: "10元", oneYearReturn: "+31.18%" },
+    { id: "14", category: "theme", name: "广发生物科技指数人民币(QDII)C", code: "016470", limitText: "10元", oneYearReturn: "+30.76%" },
 
-    { id: "33", category: "broad", name: "国泰纳斯达克100指数(QDII)", code: "160213", limitText: "0元", oneYearReturn: "+33.00%", statusText: "暂停买入", updatedAt: "08:00 06-03" },
-    { id: "34", category: "broad", name: "博时纳斯达克100ETF发起式联接(QDII)A", code: "016055", limitText: "0元", oneYearReturn: "+32.07%", statusText: "暂停买入", updatedAt: "08:00 06-03" },
-    { id: "35", category: "broad", name: "博时纳斯达克100ETF发起式联接(QDII)C", code: "016057", limitText: "0元", oneYearReturn: "+31.67%", statusText: "暂停买入", updatedAt: "08:00 06-03" },
-    { id: "36", category: "broad", name: "天弘纳斯达克100指数发起(QDII)A", code: "018043", limitText: "0元", oneYearReturn: "+31.48%", statusText: "暂停买入", updatedAt: "08:00 06-03" },
-    { id: "37", category: "broad", name: "华夏纳斯达克100ETF发起式联接(QDII)A", code: "015299", limitText: "0元", oneYearReturn: "+31.36%", statusText: "暂停买入", updatedAt: "08:00 06-03" },
-    { id: "38", category: "broad", name: "天弘纳斯达克100指数发起(QDII)C", code: "018044", limitText: "0元", oneYearReturn: "+31.23%", statusText: "暂停买入", updatedAt: "08:00 06-03" },
-    { id: "39", category: "broad", name: "华夏纳斯达克100ETF发起式联接(QDII)C", code: "015300", limitText: "0元", oneYearReturn: "+30.97%", statusText: "暂停买入", updatedAt: "08:00 06-03" },
-    { id: "40", category: "broad", name: "易方达纳斯达克100ETF联接(QDII-LOF)A", code: "161130", limitText: "0元", oneYearReturn: "+30.92%", statusText: "暂停买入", updatedAt: "08:00 06-03" },
-    { id: "41", category: "broad", name: "嘉实纳斯达克100ETF发起联接(QDII)A人民币", code: "016532", limitText: "0元", oneYearReturn: "+30.85%", statusText: "暂停买入", updatedAt: "08:00 06-03" },
-    { id: "42", category: "broad", name: "嘉实纳斯达克100ETF发起联接(QDII)C人民币", code: "016533", limitText: "0元", oneYearReturn: "+30.53%", statusText: "暂停买入", updatedAt: "08:00 06-03" },
-    { id: "43", category: "broad", name: "易方达纳斯达克100ETF联接(QDII-LOF)C", code: "012870", limitText: "0元", oneYearReturn: "+30.52%", statusText: "暂停买入", updatedAt: "08:00 06-03" }
+    { id: "15", category: "broad", name: "华安纳斯达克100ETF联接A", code: "040046", limitText: "10元", oneYearReturn: "+28.54%" },
+    { id: "16", category: "broad", name: "华安纳斯达克100ETF联接C", code: "014978", limitText: "10元", oneYearReturn: "+28.29%" },
+
+    { id: "17", category: "broad", name: "广发纳斯达克100ETF联接人民币(QDII)A", code: "270042", limitText: "10元", oneYearReturn: "+27.89%" },
+    { id: "18", category: "broad", name: "广发纳斯达克100ETF联接人民币(QDII)C", code: "006479", limitText: "10元", oneYearReturn: "+27.64%" },
+
+    { id: "19", category: "broad", name: "摩根纳斯达克100指数(QDII)人民币A", code: "019172", limitText: "10元", oneYearReturn: "+27.56%" },
+    { id: "20", category: "broad", name: "大成纳斯达克100ETF联接(QDII)A", code: "000834", limitText: "10元", oneYearReturn: "+27.50%" },
+    { id: "21", category: "broad", name: "摩根纳斯达克100指数(QDII)人民币C", code: "019173", limitText: "10元", oneYearReturn: "+27.20%" },
+    { id: "22", category: "broad", name: "大成纳斯达克100ETF联接(QDII)C", code: "008971", limitText: "10元", oneYearReturn: "+27.12%" },
+
+    { id: "23", category: "broad", name: "招商纳斯达克100ETF发起式联接(QDII)A", code: "019547", limitText: "10元", oneYearReturn: "+27.01%" },
+    { id: "24", category: "broad", name: "宝盈纳斯达克100指数发起(QDII)A人民币", code: "019736", limitText: "10元", oneYearReturn: "+26.76%" },
+    { id: "25", category: "broad", name: "华泰柏瑞纳斯达克100ETF发起式联接(QDII)A", code: "019524", limitText: "10元", oneYearReturn: "+26.66%" },
+    { id: "26", category: "broad", name: "招商纳斯达克100ETF发起式联接(QDII)C", code: "019548", limitText: "10元", oneYearReturn: "+26.52%" },
+    { id: "27", category: "broad", name: "华泰柏瑞纳斯达克100ETF发起式联接(QDII)I", code: "022664", limitText: "10元", oneYearReturn: "+26.49%" },
+    { id: "28", category: "broad", name: "宝盈纳斯达克100指数发起(QDII)C人民币", code: "019737", limitText: "10元", oneYearReturn: "+26.45%" },
+    { id: "29", category: "broad", name: "华泰柏瑞纳斯达克100ETF发起式联接(QDII)C", code: "019525", limitText: "10元", oneYearReturn: "+26.34%" },
+
+    { id: "30", category: "theme", name: "景顺长城纳斯达克科技ETF联接(QDII)A人民币", code: "017091", limitText: "0元", oneYearReturn: "+40.42%", statusText: "暂停买入" },
+    { id: "31", category: "theme", name: "景顺长城纳斯达克科技ETF联接(QDII)E人民币", code: "019118", limitText: "0元", oneYearReturn: "+40.13%", statusText: "暂停买入" },
+    { id: "32", category: "theme", name: "景顺长城纳斯达克科技ETF联接(QDII)C人民币", code: "017093", limitText: "0元", oneYearReturn: "+39.85%", statusText: "暂停买入" },
+
+    { id: "33", category: "broad", name: "国泰纳斯达克100指数(QDII)", code: "160213", limitText: "0元", oneYearReturn: "+28.78%", statusText: "暂停买入" },
+    { id: "34", category: "broad", name: "博时纳斯达克100ETF发起式联接(QDII)A", code: "016055", limitText: "0元", oneYearReturn: "+28.11%", statusText: "暂停买入" },
+    { id: "35", category: "broad", name: "博时纳斯达克100ETF发起式联接(QDII)C", code: "016057", limitText: "0元", oneYearReturn: "+27.73%", statusText: "暂停买入" },
+    { id: "36", category: "broad", name: "华夏纳斯达克100ETF发起式联接(QDII)A", code: "015299", limitText: "0元", oneYearReturn: "+27.58%", statusText: "暂停买入" },
+    { id: "37", category: "broad", name: "天弘纳斯达克100指数发起(QDII)A", code: "018043", limitText: "0元", oneYearReturn: "+27.54%", statusText: "暂停买入" },
+    { id: "38", category: "broad", name: "天弘纳斯达克100指数发起(QDII)C", code: "018044", limitText: "0元", oneYearReturn: "+27.29%", statusText: "暂停买入" },
+    { id: "39", category: "broad", name: "华夏纳斯达克100ETF发起式联接(QDII)C", code: "015300", limitText: "0元", oneYearReturn: "+27.21%", statusText: "暂停买入" },
+    { id: "40", category: "broad", name: "嘉实纳斯达克100ETF发起联接(QDII)A人民币", code: "016532", limitText: "0元", oneYearReturn: "+27.09%", statusText: "暂停买入" },
+    { id: "41", category: "broad", name: "易方达纳斯达克100ETF联接(QDII-LOF)A", code: "161130", limitText: "0元", oneYearReturn: "+27.03%", statusText: "暂停买入" },
+    { id: "42", category: "broad", name: "嘉实纳斯达克100ETF发起联接(QDII)C人民币", code: "016533", limitText: "0元", oneYearReturn: "+26.77%", statusText: "暂停买入" },
+    { id: "43", category: "broad", name: "易方达纳斯达克100ETF联接(QDII-LOF)C", code: "012870", limitText: "0元", oneYearReturn: "+26.64%", statusText: "暂停买入" }
   ]
 };
